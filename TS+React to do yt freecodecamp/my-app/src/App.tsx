bab3172d7cf46524a9fe01cf951270982e47e31a
Amanpreet Taluja <87584606+Amanpreet-Taluja@users.noreply.github.com>
2022-02-16 16:49:15 +0530

    16-02-22 16:47
    ToDo List added with edit remove and isDone functionality
    UseRef used for focusing and blurring
    Inline functions in Typescript React explored

--- TS+React to do yt freecodecamp/my-app/src/App.tsx
+++ TS+React to do yt freecodecamp/my-app/src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputField from './components/InputField'
 import './App.css';
+import { Todo } from './components/model';
+import Todolist from './components/Todolist';
 
 const App:React.FC=()=> {
+  const [todo,setTodo]=useState<string>('');
+  const [todos,setTodos]=useState<Todo[]>([]);
+
+  const handleAdd=(event:React.FormEvent)=>{
+    event.preventDefault();
+    if(todo){
+      setTodos([...todos,{id:Date.now(),todo:todo,isDone:false}]);
+      setTodo('');
+    }
+  }
+  console.log(todos);
   return (
     <div className="App">
       <span className="heading">Taksify</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <Todolist todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
