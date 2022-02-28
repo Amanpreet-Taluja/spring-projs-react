470bf1d6d07185edd91b40a47e26c3a1e5b83cfe
Amanpreet Taluja <87584606+Amanpreet-Taluja@users.noreply.github.com>
2022-02-28 11:37:19 +0530

    Built an autocomplete text box

---
+++ yt freecodeCamp autocomplete app/new/src/AutoCompleteText.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import './AutoCompleteText.css'
+const AutoCompleteText = ({items}) => {
+  const [suggestions, setSuggestions] = useState([]);
+  const [text,setText]=useState('');
+
+  const onTextChanged = (e) => {
+    const value = e.target.value;
+    if (value.length === 0) {
+      setSuggestions([]);
+    } else {
+      const regex = new RegExp(`^${value}`, "i");
+      const a = items.sort().filter((item) => item.match(regex));
+      setSuggestions(a);
+    }
+    setText(value);
+  };
+  const suggestionSelected=(value)=>{
+      setText(value);
+      setSuggestions([]);
+  }
+
+  return (
+    <div className="AutoCompleteText">
+      <input value={text}type="text" onChange={onTextChanged} />
+      {suggestions.length !== 0 && (
+        <ul>
+          {suggestions.map((suggestion) => (
+            <li onClick={()=>suggestionSelected(suggestion)}>{suggestion}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default AutoCompleteText;
