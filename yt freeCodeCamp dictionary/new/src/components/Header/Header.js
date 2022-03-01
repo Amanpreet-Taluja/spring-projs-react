5948105a9a696f4915ad5dc850e5542052292657
Amanpreet Taluja <87584606+Amanpreet-Taluja@users.noreply.github.com>
2022-03-01 10:37:17 +0530

    Basic dictionary app without styling

---
+++ yt freeCodeCamp dictionary/new/src/components/Header/Header.js
@@ -0,0 +1,55 @@
+import {
+  createTheme,
+  MenuItem,
+  TextField,
+  ThemeProvider,
+} from "@material-ui/core";
+import React from "react";
+import "./Header.css";
+import categories from "../../data/category";
+const Header = ({ category, setCategory, word, setWord }) => {
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  const handleCatChange=(value)=>{
+    setCategory(value);
+    setWord('');
+  }
+  return (
+    <div className="header">
+      <span className="title">{word ? word : "Word Hunt"}</span>
+      <div className="inputs">
+        <ThemeProvider theme={darkTheme}>
+          <TextField
+            className="search"
+            label="Search a word"
+            id="standard-basic"
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+          />
+          <TextField
+            select
+            className="select"
+            label="Language"
+            value={category}
+            onChange={(e) =>handleCatChange(e.target.value) }
+          >
+            {categories.map((option) => (
+              <MenuItem key={option.label} value={option.label}>
+                {option.value}
+              </MenuItem>
+            ))}
+          </TextField>
+        </ThemeProvider>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
