fb1acef3c581e230182e568680efc6fb1740452a
Amanpreet Taluja <87584606+Amanpreet-Taluja@users.noreply.github.com>
2022-03-17 16:25:25 +0530

    Chnaged hard coding in config

--- copy trial amigos/demo/src/main/java/com/example/demo/student/PetConfig.java
+++ copy trial amigos/demo/src/main/java/com/example/demo/student/PetConfig.java
@@ -8,29 +8,5 @@ import java.util.List;
 
 @Configuration
 public class PetConfig {
-    @Bean
-    CommandLineRunner commandLineRunner(PetRepository repository){
-        return args -> {
-            Pet mariam = new Pet(
-                    1,
-                    1,
-                    4,
-                    "24th",
-                    "done",
-                    true
-            );
-            Pet alex = new Pet(
-                    2,
-                    2,
-                    5,
-                    "25th",
-                    "transit",
-                    false
-            );
-
-            repository.saveAll(
-                    List.of(mariam,alex)
-            );
-        };
-    }
+  
 }
