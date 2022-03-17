26bdfb54d641a8af9af0379fbdb67d445cd4f70a
Amanpreet Taluja <87584606+Amanpreet-Taluja@users.noreply.github.com>
2022-03-17 16:22:48 +0530

    Working for Pet fetching and saving

--- copy trial amigos/demo/src/main/java/com/example/demo/student/PetController.java
+++ copy trial amigos/demo/src/main/java/com/example/demo/student/PetController.java
@@ -2,7 +2,8 @@ package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import java.util.List;
 
 
@@ -25,6 +26,25 @@ public class PetController {
     @PostMapping
     public void RegisterNewPet(@RequestBody Pet pet){
         petService.addPet(pet);
+    }
+
+    @GetMapping(value = "/getapi")
+    public String getPetsFromApi(){
+        String url="https://petstore.swagger.io/v2/store/order/8";
+        WebClient client = WebClient.create();
+        WebClient.ResponseSpec responseSpec = client.get()
+                .uri(url)
+                .retrieve();
+        Mono<Pet> responseBody = responseSpec.bodyToMono(Pet.class);
+        Integer id=responseBody.map(pet -> pet.getId()).block();
+        Integer petId=responseBody.map(pet -> pet.getPetId()).block();
+        Integer quantity=responseBody.map(pet -> pet.getQuantity()).block();
+        String shipDate=responseBody.map(pet -> pet.getShipDate()).block();
+        String status=responseBody.map(pet -> pet.getStatus()).block();
+        Boolean complete=responseBody.map(pet -> pet.getComplete()).block();
+        Pet p=new Pet(id,petId,quantity,shipDate,status,complete);
+        petService.addPet(p);
+        return shipDate;
     }
 
 }
